f93cc84e7791468be3dcc2c59d81563266274b60
estevaoMG <estevao.gouveia@hotmail.com>
2024-08-20 22:57:12 -0300

    Menu iterativo

--- Program.cs
+++ Program.cs
@@ -1,19 +1,133 @@
 ﻿using ExemploFundamentos.Models;
 
 
-int numero = 5;
-int contador = 1;
 
-while (contador <= 10)
+string opcao;
+bool exibirMenu = true;
+
+while(exibirMenu)
 {
-    Console.WriteLine($" {contador}° Execução: {numero} x {contador} = {numero * contador}");
-    contador++;
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
 
-    if (contador == 6)
+    opcao = Console.ReadLine();
+
+    switch(opcao)
     {
+        case "1":
+        Console.WriteLine("Cadastro de cliente");
         break;
+
+        case "2":
+        Console.WriteLine("Busca de cliente");
+        break;
+
+        case "3":
+        Console.WriteLine("Apagar cliente");
+        break;
+
+        case "4":
+        Console.WriteLine("Encerrar");
+        exibirMenu = false;
+        //Environment.Exit(0);
+        break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
 }
+
+Console.WriteLine("O programa se encerrou");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int soma = 0, numero = 0;
+
+
+// do
+// {
+//     Console.WriteLine("Digite um número (0 para parar):");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+
+// } while(numero != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 1;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($" {contador}° Execução: {numero} x {contador} = {numero * contador}");
+//     contador++;
+
+//     if (contador == 6)
+//     {
+//         break;
+//     }
+// }
 
 
 
