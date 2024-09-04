2cba5e6051ec594ec9b536a643b80e34885e7ed2
estevaoMG <estevao.gouveia@hotmail.com>
2024-09-04 17:54:13 -0300

    Percorrendo um array com FOREACH

--- Program.cs
+++ Program.cs
@@ -8,13 +8,20 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 arrayInteiros[3] = 1;
 
+Console.WriteLine("Percorrendo o Array com o FOR");
 for( int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
 }
 
+Console.WriteLine("Percorrendo o Array com o FOREACH");
 
-
+int contadorForeach = 0;
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {valor}");
+    contadorForeach++;
+}
 
 
 
